cb8e39bb42265043cf905543c53fa18bfe6dfb60
Samet Yıldırım <samety3503@gmail.com>
2021-03-08 02:24:01 +0300

    6

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -8,9 +6,10 @@ const Comment = require("./models/Comment");
 const dbConnect = require("./utils/dbConnect");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 app.use(cors());
-
 app.use(express.json());
 dbConnect();
 app.get("/", (req, res) => {
