714bebcb9176e563ea9a40c7ee8cefd46a0b7bc0
Samet Yıldırım <samety3503@gmail.com>
2021-03-08 02:27:12 +0300

    7

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const jwt = require("jsonwebtoken");
 const Comment = require("./models/Comment");
 const dbConnect = require("./utils/dbConnect");
 const nodemailer = require("nodemailer");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+});
 app.use(express.json());
 dbConnect();
 app.get("/", (req, res) => {
